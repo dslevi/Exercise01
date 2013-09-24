c5b012997c913285e2c01cdb634e2bebd2b4321c
Hackbright Student <user@chromebox-005.(none)>
2013-09-24 16:06:22 -0700

    Guessing game with isNumber function

--- guess.py
+++ guess.py
@@ -5,10 +5,19 @@ tries = 0
 
 name = raw_input("Howdy, what's your name? ")
 print "%s, I'm thinking of a number between 1 and 100.  Try to guess my number." % name
+print num
+
+def isNumber(guess):
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    for x in guess:
+        if x not in numbers:
+            return False
+    return True
 
 while True:
     guess = raw_input("Your guess? ")
-    if guess.isdigit():
+    if isNumber(guess):
         guess = int(guess)
         tries += 1
         if guess > num:
