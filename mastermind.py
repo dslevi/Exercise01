73534f79ec602eecddf3b9b7c66692959ce6af64
Danielle Levi <dslevi12@gmail.com>
2013-09-24 23:21:07 -0700

    Shows feedback if number OR placement is correct

--- mastermind.py
+++ mastermind.py
@@ -1,47 +1,49 @@
 import random
 
 turns = 12
-
 master = []
 for x in range(4):
     master.append(random.randint(1, 4))
 
 name = raw_input("What's your name? ")
 
-print "Hello %s, welcome to mastermind!" % name
+print "Hello, %s. Welcome to Mastermind!" % name
 print """Mastermind is a code-breaking game.
-    You will try to guess the pattern in both order and number.
-    You get 12 turns to guess the sequence of 4 numbers, 
-    each ranging from 1 to 4.
+    You have 12 turns to guess a mystery pattern of four numbers.
+    Each digit in this sequence ranges from 1 to 4.
     Enter your guess in order, one number at a time.
     
-    If your number AND placement both matche the master sequence,
+    If your number AND placement both match the master sequence,
     an "O" will be displayed.
-    If only your number OR placement is correct,
+    If either your number OR placement is correct,
     a "X" will be displayed.
-    If your number and placement is incorrect,
-    a "-" will be displayed.
+    If your number and placement are both incorrect,
+    a "*" will be displayed.
 
     Good luck!
     """
-print master
-
+    
 for tries in range(turns):
     print "Attempt: %d" % (tries + 1)
     attempt = []
-
+    tempMaster = master[:]
+	
     for place in range(4):
         num = int(raw_input("> "))
-        if num == master[place]:
+    	if num == master[place]:
             attempt.append("O")
+            tempMaster[place] = "O"
         else:
-            for x in range(4):
-                if num == master[x]:
-                    attempt.append("X")
-                    break
-            else:
-                attempt.append("-")
-    print attempt
+            attempt.append(num)
+    
+    for y in attempt:
+    	if y != "O":
+			for z in range(4):
+				if y == tempMaster[z]:
+					attempt[attempt.index(y)] = "X"
+					break
+    			else:
+    				attempt[attempt.index(y)] = "*"
     
     right = 0
     for x in attempt:
@@ -52,6 +54,6 @@ for tries in range(turns):
         print "Congratulations! You have won!"
         break
     if tries == (turns - 1):
-        print "Sorry you lost. Try again?"
+        print "Sorry, you lost. :("
 
 
